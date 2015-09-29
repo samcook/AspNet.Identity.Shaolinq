a71c6a27fd6909bd2a291023873dde1c2c986f9e
Sam Cook <github@sam.net.nz>
2015-09-29 10:55:04 +0100

    Update Shaolinq version and nuget package version

--- AspNet.Identity.Shaolinq/Properties/AssemblyInfo.cs
+++ AspNet.Identity.Shaolinq/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AspNet.Identity.Shaolinq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A persistence provider for AspNet Identity using Shaolinq")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AspNet.Identity.Shaolinq")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Sam Cook 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha2")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0-alpha1")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
